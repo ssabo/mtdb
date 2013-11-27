5a6ec9adda3bcf423563fcb9e0f764042e706306
Shaun Sabo <shaun.sabo@gmail.com>
2013-11-27 01:10:21 -0800

    initial comments

--- print_card.py
+++ print_card.py
@@ -3,10 +3,11 @@
 import urllib2
 from bs4 import BeautifulSoup
 
-
+# global objects
 URL = 'http://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid=31790'
 soup = BeautifulSoup(urllib2.urlopen(URL))
 
+# HTML Object IDs
 name_id   = 'ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_nameRow'
 mana_id   = 'ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_manaRow'
 cmc_id    = 'ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_cmcRow'
@@ -19,6 +20,7 @@ rarity_id = 'ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_rarityRow'
 artist_id = 'ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_artistRow'
 card_id   = 'ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_numberRow'
 
+# extracts the value items from the specified section
 def extract_items(soup,content_id):
   bs4content = soup.find(id=content_id)
   if bs4content != None:
@@ -27,6 +29,7 @@ def extract_items(soup,content_id):
   else:
     return
 
+# extracts and formats regular content with or without images
 def extract_content(soup,content_id, delim = ' '):
   bs4content = extract_items(soup,content_id)
   if bs4content == None:
@@ -51,25 +54,26 @@ def extract_content(soup,content_id, delim = ' '):
       
   return content.strip()
 
+# extracts raw text from content ignoring images
 def extract_text(soup,content_id):
   bs4content = extract_items(soup,content_id)
   bs4strings = bs4content.stripped_strings
   return ' '.join(string for string in bs4strings).strip()
 
+# extracts mana values from contents
 def extract_mana(soup,content_id):
   bs4content = extract_items(soup,content_id)
   bs4images = bs4content.find_all('img')
   return ' '.join(image['alt'] for image in bs4images).strip()
 
-if __name__ == "__main__":
-  print "Name      :    %s" % extract_content(soup,name_id)
-  print "Mana      :    %s" % extract_mana(soup,mana_id)
-  print "CMC       :    %s" % extract_content(soup,cmc_id)
-  print "Types     :    %s" % extract_content(soup,types_id)
-  print "Text      :    %s" % extract_content(soup,text_id,'\n')
-  print "Flavor    :    %s" % extract_content(soup,flavor_id)
-  print "P/T       :    %s" % extract_content(soup,p_t_id)
-  print "Expansion :    %s" % extract_text(soup,set_id)
-  print "Rarity    :    %s" % extract_content(soup,rarity_id)
-  print "Card ID   :    %s" % extract_content(soup,card_id)
-  print "Artist    :    %s" % extract_content(soup,artist_id)
+print "Name      :    %s" % extract_content(soup,name_id)
+print "Mana      :    %s" % extract_mana(soup,mana_id)
+print "CMC       :    %s" % extract_content(soup,cmc_id)
+print "Types     :    %s" % extract_content(soup,types_id)
+print "Text      :    %s" % extract_content(soup,text_id,'\n')
+print "Flavor    :    %s" % extract_content(soup,flavor_id)
+print "P/T       :    %s" % extract_content(soup,p_t_id)
+print "Expansion :    %s" % extract_text(soup,set_id)
+print "Rarity    :    %s" % extract_content(soup,rarity_id)
+print "Card ID   :    %s" % extract_content(soup,card_id)
+print "Artist    :    %s" % extract_content(soup,artist_id)
